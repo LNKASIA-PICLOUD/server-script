394f0d044990d4ca2043e5245768bd00e41285aa
dhaval-lnkasia <dhaval@lnkasia.com>
2025-04-16 22:07:36 +0530

    Update picloud.bash

--- picloud.bash
+++ picloud.bash
@@ -33,15 +33,24 @@ echo -e "\e[32mThanks for using this script...\e[0m"
 sleep 2
 
 # Variables
-OWNCLOUD_DIR="/var/www/owncloud"
-DB_ROOT_PASS="1234"
-OWNCLOUD_ADMIN_USER="root"
-OWNCLOUD_ADMIN_PASS="1234"
+PICLOUD_DIR="/var/www/picloud"
+DB_ROOT_PASS="Lnkasia#2025"
+PICLOUD_ADMIN_USER="admin"
+PICLOUD_ADMIN_PASS="Lnkasia@2025"
 REPO_URL="https://github.com/LnkAsia/picloudserver.git"
 
 # Update packages
 apt update && apt install -y \
   apache2 mariadb-server curl unzip gnupg2 php-pear software-properties-common
+
+apt update
+apt install -y software-properties-common
+add-apt-repository ppa:ondrej/php -y
+apt update
+
+apt install -y php7.4 libapache2-mod-php7.4 \
+  php7.4-{cli,fpm,common,mbstring,xml,gd,curl,intl,mysql,zip,bcmath}
+
 
 # Add PHP 7.4 (if not already)
 add-apt-repository ppa:ondrej/php -y
@@ -49,19 +58,19 @@ apt update
 apt install -y php7.4 libapache2-mod-php7.4 php7.4-{mysql,intl,curl,json,gd,xml,mbstring,zip}
 
 # Clone ownCloud server repository
-rm -rf "$OWNCLOUD_DIR"
-git clone --depth 1 "$REPO_URL" "$OWNCLOUD_DIR"
+rm -rf "$PICLOUD_DIR"
+git clone --depth 1 "$REPO_URL" "$PICLOUD_DIR"
 
 # Set permissions
-cd "$OWNCLOUD_DIR"
+cd "$PICLOUD_DIR"
 chown -R www-data:www-data .
 chmod -R 755 .
 
 # Apache configuration
 cat > /etc/apache2/sites-available/owncloud.conf << EOL
-Alias / "/var/www/owncloud/"
+Alias / "/var/www/picloud/"
 
-<Directory /var/www/owncloud/>
+<Directory /var/www/picloud/>
   Options +FollowSymlinks
   AllowOverride All
 
@@ -69,8 +78,8 @@ Alias / "/var/www/owncloud/"
     Dav off
   </IfModule>
 
-  SetEnv HOME /var/www/owncloud
-  SetEnv HTTP_HOME /var/www/owncloud
+  SetEnv HOME /var/www/picloud
+  SetEnv HTTP_HOME /var/www/picloud
 </Directory>
 EOL
 
@@ -81,18 +90,18 @@ systemctl restart apache2
 
 # Setup MariaDB
 mysql -u root <<EOF
-CREATE DATABASE ownclouddb;
-GRANT ALL PRIVILEGES ON ownclouddb.* TO 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';
+CREATE DATABASE piclouddb;
+GRANT ALL PRIVILEGES ON piclouddb.* TO 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';
 FLUSH PRIVILEGES;
 EOF
 
 # Run ownCloud installation
 sudo -u www-data php $OWNCLOUD_DIR/occ maintenance:install \
   --database "mysql" \
-  --database-name "ownclouddb" \
-  --database-user "root" \
+  --database-name "piclouddb" \
+  --database-user "admin" \
   --database-pass "${DB_ROOT_PASS}" \
   --admin-user "${OWNCLOUD_ADMIN_USER}" \
   --admin-pass "${OWNCLOUD_ADMIN_PASS}"
 
-echo -e "\e[32mownCloud successfully installed from latest ZIP!\e[0m"
+echo -e "\e[32mpiCloud successfully installed from latest ZIP!\e[0m"
